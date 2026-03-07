68d5327ee327e4759bd583a9a3c35477cdde7fc4
wartachter-byte <wartachter@gmail.com>
2026-03-07 09:50:37 +0100

    Refactor Loggable interface formatting

--- src/net/minecraft/util/Loggable.java
+++ src/net/minecraft/util/Loggable.java
@@ -3,9 +3,3 @@ package net.minecraft.util;
 public interface Loggable {
   void log(String paramString);
 }
-
-
-/* Location:              C:\Users\thijs\Download\\unstitcher.jar!\net\minecraf\\util\Loggable.class
- * Java compiler version: 6 (50.0)
- * JD-Core Version:       1.1.3
- */
